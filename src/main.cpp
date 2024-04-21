dd8423d26309c89f44f84c9b6b727c669a9b76e3
markus <ms@dom.de>
2024-04-21 22:48:13 +0200

    working dhcpoption 251

--- src/main.cpp
+++ src/main.cpp
@@ -1,133 +1,60 @@
-/*
- Basic MQTT example
+// This demo does web requests via DHCP and DNS lookup.
+// 2011-07-05 <jc@wippler.nl>
+//
+// License: GPLv2
 
- This sketch demonstrates the basic capabilities of the library.
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic"
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
+#include <EtherCard.h>
 
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
- 
-*/
+#define REQUEST_RATE 5000 // milliseconds
 
-#include <SPI.h>
-#include <EthernetENC.h>
-#include <PubSubClient.h>
+// ethernet interface mac address
+static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+// remote website name
+char mqtt[30] = "mqtt.muebaschu.org";
 
-// Update these with values suitable for your network.
-byte mac[]    = { 0x06, 0x00, 0x00, 0x00, 0x00, 0x01 };
-IPAddress server(172, 16, 133, 1);
+byte Ethernet::buffer[700];
+static long timer;
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i=0;i<length;i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-}
-
-EthernetClient ethClient;
-PubSubClient client(ethClient);
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("arduinoClient")) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("outTopic","hello world");
-      // ... and resubscribe
-      client.subscribe("inTopic");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
+void DHCPOption(uint8_t option, const byte* data, uint8_t len) {
+  if (option == 251) {
+    strcpy(mqtt, (char*)data);
+    mqtt[len] = 0;
   }
 }
 
-void setup()
-{
-  Serial.begin(9600);
+void setup () {
+  Serial.begin(57600);
+  Serial.println("\n[getDHCPandDNS]");
 
-  client.setServer(server, 1883);
-  client.setCallback(callback);
+  ether.dhcpAddOptionCallback(251,DHCPOption);
 
-  Ethernet.dhcpAddOptionCallback(246,DHCPOption); 
+  // Change 'SS' to your Slave Select pin, if you arn't using the default pin
+  if (ether.begin(sizeof Ethernet::buffer, mymac, SS) == 0)
+    Serial.println( "Failed to access Ethernet controller");
 
-  Ethernet.begin(mac);
-  // Allow the hardware to sort itself out
-  delay(1500);
-  Serial.print("My IP address: ");
-  Serial.println(Ethernet.localIP());
+  if (!ether.dhcpSetup())
+    Serial.println("DHCP failed");
+
+  ether.printIp("My IP: ", ether.myip);
+  // ether.printIp("Netmask: ", ether.mymask);
+  ether.printIp("GW IP: ", ether.gwip);
+  ether.printIp("DNS IP: ", ether.dnsip);
+
+  Serial.println(mqtt);
+
+  if (!ether.dnsLookup(mqtt, true))
+    Serial.println("DNS failed");
+  ether.printIp("Server: ", ether.hisip);
+
+  timer = - REQUEST_RATE; // start timing out right away
 }
 
-void loop()
-{
-  switch (Ethernet.maintain()) {
+void loop () {
 
-    case 1:
+  ether.packetLoop(ether.packetReceive());
 
-      //renewed fail
-
-      Serial.println("Error: renewed fail");
-
-      break;
-
-    case 2:
-
-      //renewed success
-
-      Serial.println("Renewed success");
-
-      //print your local IP address:
-
-      Serial.print("My IP address: ");
-
-      Serial.println(Ethernet.localIP());
-
-      break;
-
-    case 3:
-
-      //rebind fail
-
-      Serial.println("Error: rebind fail");
-
-      break;
-
-    case 4:
-
-      //rebind success
-
-      Serial.println("Rebind success");
-
-      //print your local IP address:
-
-      Serial.print("My IP address: ");
-
-      Serial.println(Ethernet.localIP());
-
-      break;
-
-    default:
-
-      //nothing happened
-
-      break;
-
+  if (millis() > timer + REQUEST_RATE) {
+    timer = millis();
+    Serial.println("\n>>> REQ");
   }
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
 }
