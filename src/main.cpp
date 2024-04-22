87f72e24d86036fea5cbab37753a1075d13289d8
markus <ms@dom.de>
2024-04-22 10:10:53 +0200

    assembly lacks ethClient

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 //
 // License: GPLv2
 
+#include <SPI.h>
 #include <EtherCard.h>
+#include <PubSubClient.h>
 
 #define REQUEST_RATE 5000 // milliseconds
 
@@ -14,6 +16,40 @@ char mqtt[30] = "mqtt.muebaschu.org";
 
 byte Ethernet::buffer[700];
 static long timer;
+
+EtherCard ethClient;
+PubSubClient client(ethClient);
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("arduinoClient")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic","hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void sub_callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
 
 void DHCPOption(uint8_t option, const byte* data, uint8_t len) {
   if (option == 251) {
@@ -46,9 +82,26 @@ void setup () {
     Serial.println("DNS failed");
   ether.printIp("Server: ", ether.hisip);
 
+  client.setServer(ether.hisip, 1883);
+  client.setCallback(sub_callback);
+
+
+
+
+
   timer = - REQUEST_RATE; // start timing out right away
 }
 
+
+void loop()
+{
+ if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
+
+/*
 void loop () {
 
   ether.packetLoop(ether.packetReceive());
@@ -58,3 +111,19 @@ void loop () {
     Serial.println("\n>>> REQ");
   }
 }
+
+
+ Basic MQTT example
+
+ This sketch demonstrates the basic capabilities of the library.
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic"
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+ 
+*/
+
